56a39fd58d0053bec0e706ecd5681d3b2a2aa12d
aboofainaz <aboofainaz@gmail.com>
2025-11-26 22:00:39 +0530

    feat: Implement initial React app structure with custom logo and welcome message.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './custom_logo.svg';
 import './App.css';
 
 function App() {
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello! Welcome to my First React Project
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
